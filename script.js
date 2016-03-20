dc049b360fca316a858406e7d71c08f8e526aa6d
Emma Grapek <emmagrapek@gmail.com>
2016-03-19 20:25:07 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+ 
+    $(".dig-paint-link").click(function(){
+    $(".digital-paintings").hide();
+});
+
+$(".dig-paint-link").click(function(){
+    $(".digital-paintings").show();
+});
+ 
+});
